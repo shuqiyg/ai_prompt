41680d2c0d4cca25b72ec8017bfc87004b27538b
wweerrbb <ysq13579@hotmail.com>
2023-05-16 23:26:12 -0400

    adding images and assets

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,7 +9,11 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href="/" className="flex gap-2 flex-center">
-            <Image />
+            <Image src="/assets/images/commentLogo.svg"
+            alt="PromptHero Logo"
+            width={40}
+            height={40}/> 
+            <p className="logo_text">PromptHero</p>    
         </Link>
     </nav>
   )
